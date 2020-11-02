9ed1f25e703ade2db388aed30e3605ac0e371d21
Augusto Bennemann <augusto@augusto.dev>
2020-11-01 23:46:18 -0300

    Add shadow to button

--- pages/_error.js
+++ pages/_error.js
@@ -6,7 +6,7 @@ function Error({ statusCode }) {
     <div className="h-screen text-center flex flex-col items-center justify-center">
       <h2 className="font-mono">Ops, a conexão micelial se rompeu.</h2>
       <Link href={`/`}>
-        <button className="bg-black hover:bg-white text-white hover:text-black py-1 px-6 mt-5 font-mono">
+        <button className="bg-black hover:bg-white text-white hover:text-black py-1 px-6 mt-5 font-mono shadow-md">
           ver fluxus
         </button>
       </Link>
