99ca7ad6b83ffaf518f971b23cb7e9bdaa44b8cd
Augusto Bennemann <augusto@augusto.dev>
2020-11-01 23:44:01 -0300

    Add error page and change page titles

---
+++ pages/_error.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import Link from 'next/link'
+
+function Error({ statusCode }) {
+  return (
+    <div className="h-screen text-center flex flex-col items-center justify-center">
+      <h2 className="font-mono">Ops, a conexão micelial se rompeu.</h2>
+      <Link href={`/`}>
+        <button className="bg-black hover:bg-white text-white hover:text-black py-1 px-6 mt-5 font-mono">
+          ver fluxus
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
