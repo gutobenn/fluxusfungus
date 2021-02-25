8595e69edcb21ea7219a7763d99ab2ce4ebf4116
Augusto Bennemann <augusto@augusto.dev>
2021-02-25 20:40:15 -0300

    Change text color

--- components/header.js
+++ components/header.js
@@ -20,21 +20,21 @@ export default function Header() {
       >
         <nav className="flex items-center justify-between flex-wrap bg-transparent p-3 flex fixed top-0 inset-x-0 items-center xl:h-full xl:w-1/5 xl:flex-row xl:items-stretch opacity-0">
           <div className="flex container mx-auto xl:flex-col">
-            <div className="flex items-center flex-shrink-0 text-black mr-6 xl:ml-8">
+            <div className="flex items-center flex-shrink-0 text-white mr-6 xl:ml-8">
               <span className="font-semibold text-xl text-primary tracking-tight leading-tight font-mono xl:text-7xl xl:mt-20">
                 fluxus fungus
               </span>
             </div>
-            <div className="w-full block flex-grow flex items-center w-auto xl:flex-col xl:items-start xl:ml-8 xl:mb-20 font-mono">
+            <div className="w-full block flex-grow flex items-center w-auto xl:flex-col xl:items-start xl:ml-8 xl:mb-20 font-mono text-gray-300">
               <div className="text-sm flex-grow"></div>
               <div>
                 {router.locale === 'pt' ? (
                   <Link href={`/?page=sobre`} as={'/sobre'} scroll={false}>
-                    <a className="block mt-0 hover:text-black mr-2">sobre</a>
+                    <a className="block mt-0 hover:text-white mr-2">sobre</a>
                   </Link>
                 ) : (
                   <Link href={`/?page=about`} as={'/about'} scroll={false}>
-                    <a className="block mt-0 hover:text-black mr-2">about</a>
+                    <a className="block mt-0 hover:text-white mr-2">about</a>
                   </Link>
                 )}
               </div>
@@ -45,7 +45,7 @@ export default function Header() {
                     as={'/contaminacoes'}
                     scroll={false}
                   >
-                    <a className="block mt-0 hover:text-black mr-2">
+                    <a className="block mt-0 hover:text-white mr-2">
                       contaminações
                     </a>
                   </Link>
@@ -55,18 +55,18 @@ export default function Header() {
                     as={'/contaminations'}
                     scroll={false}
                   >
-                    <a className="block mt-0 hover:text-black mr-2">
+                    <a className="block mt-0 hover:text-white mr-2">
                       contaminations
                     </a>
                   </Link>
                 )}
               </div>
-              <div className="text-sm ml-5 xl:mt-6 xl:ml-0 fixed bottom-0 right-0 mb-3 mr-4 xl:relative xl:mb-0 xl:mr-0 text-gray-800">
+              <div className="text-sm ml-5 xl:mt-6 xl:ml-0 fixed bottom-0 right-0 mb-3 mr-4 xl:relative xl:mb-0 xl:mr-0 text-gray-400">
                 {router.locale === 'pt' ? (
                   <>
                     [<span className="underline">pt</span>|
                     <Link href={`/`} locale="en">
-                      <a className="block mt-0 hover:text-black inline-block">
+                      <a className="block mt-0 hover:text-white inline-block">
                         en
                       </a>
                     </Link>
@@ -76,7 +76,7 @@ export default function Header() {
                   <>
                     [
                     <Link href={`/`} locale="pt">
-                      <a className="block mt-0 hover:text-black inline-block">
+                      <a className="block mt-0 hover:text-white inline-block">
                         pt
                       </a>
                     </Link>
